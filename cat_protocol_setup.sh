c72615cd642bbfa5820838b203f5198a99a66317
fcrre26 <123922394+fcrre26@users.noreply.github.com>
2024-09-12 21:14:11 +0800

    Update cat_protocol_setup.sh

--- cat_protocol_setup.sh
+++ cat_protocol_setup.sh
@@ -268,7 +268,7 @@ EOL
         const mnemonic = '$MNEMONIC';
         const seed = bip39.mnemonicToSeedSync(mnemonic);
         const root = bitcoin.bip32.fromSeed(seed);
-        const account = root.derivePath('$ACCOUNT_PATH'.replace(/'/g, \"\\'\"));
+        const account = root.derivePath('m/86\'/0\'/0\'/0/0');
         console.log(account.toWIF());
     ")
 
@@ -279,7 +279,7 @@ EOL
         const mnemonic = '$MNEMONIC';
         const seed = bip39.mnemonicToSeedSync(mnemonic);
         const root = bitcoin.bip32.fromSeed(seed);
-        const account = root.derivePath('$ACCOUNT_PATH'.replace(/'/g, \"\\'\"));
+        const account = root.derivePath('m/86\'/0\'/0\'/0/0');
         const { address } = payments.p2tr({ pubkey: account.publicKey });
         console.log(address);
     ")
